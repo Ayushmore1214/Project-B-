b5ee86787471bcd8453b30b56bfb5e96a8c7227f
Ayush More <ayushmore42595@gmail.com>
2025-06-16 19:59:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,3 +6,5 @@ SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///backup.db')
 GITHUB_CLIENT_ID = os.getenv('GITHUB_CLIENT_ID', 'dummy-id')
 GITHUB_CLIENT_SECRET = os.getenv('GITHUB_CLIENT_SECRET', 'dummy-secret')
 GCS_BUCKET_NAME = os.getenv('GCS_BUCKET_NAME', 'your-default-bucket-name')
+SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+
