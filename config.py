19ff50db2f09b072de9c55e4ed69eadb08243621
Ayush More <ayushmore42595@gmail.com>
2025-06-16 20:04:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,5 +6,6 @@ SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///backup.db')
 GITHUB_CLIENT_ID = os.getenv('GITHUB_CLIENT_ID', 'dummy-id')
 GITHUB_CLIENT_SECRET = os.getenv('GITHUB_CLIENT_SECRET', 'dummy-secret')
 GCS_BUCKET_NAME = os.getenv('GCS_BUCKET_NAME', 'your-default-bucket-name')
-SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+DATABASE_URL=postgresql+psycopg2://<USER>:<PASSWORD>@/<DB_NAME>?host=/cloudsql/<CONNECTION_NAME>
+
 
