1a6994c91382339498a738ea22ba67ecfe1808a7
Ayush More <ayushmore42595@gmail.com>
2025-06-16 19:56:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,8 @@
 import os
 
 class Config:
-    SECRET_KEY = os.getenv('FLASK_SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    GITHUB_CLIENT_ID = os.getenv('GITHUB_CLIENT_ID')
-    GITHUB_CLIENT_SECRET = os.getenv('GITHUB_CLIENT_SECRET')
-    GCS_BUCKET_NAME = os.getenv('GCS_BUCKET_NAME')
+    SECRET_KEY = os.getenv('FLASK_SECRET_KEY', 'dev-secret-key')
+SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///backup.db')
+GITHUB_CLIENT_ID = os.getenv('GITHUB_CLIENT_ID', 'dummy-id')
+GITHUB_CLIENT_SECRET = os.getenv('GITHUB_CLIENT_SECRET', 'dummy-secret')
+GCS_BUCKET_NAME = os.getenv('GCS_BUCKET_NAME', 'your-default-bucket-name')
