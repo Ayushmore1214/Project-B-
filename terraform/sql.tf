d1d644806dc1151d6819922eeed34d9bca5a0364
Ayush More <ayushmore42595@gmail.com>
2025-06-17 22:55:47 +0530

    Update sql.tf

--- terraform/sql.tf
+++ terraform/sql.tf
@@ -5,7 +5,7 @@ resource "google_sql_database_instance" "main" {
   settings {
     tier = "db-f1-micro"
     ip_configuration {
-      ipv4_enabled = false
+      ipv4_enabled = true
     }
   }
   deletion_protection = false
