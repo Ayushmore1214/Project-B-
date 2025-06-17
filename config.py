f833f6ad8b4fc6ffbbaf7c6274bcc723cbcc8048
Ayush More <ayushmore42595@gmail.com>
2025-06-17 15:39:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ import os
 class Config:
     SECRET_KEY = os.getenv('FLASK_SECRET_KEY', 'dev-secret-key')
 SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///backup.db')
-GITHUB_CLIENT_ID = os.getenv('GITHUB_CLIENT_ID', 'dummy-id')
-GITHUB_CLIENT_SECRET = os.getenv('GITHUB_CLIENT_SECRET', 'dummy-secret')
+GITHUB_CLIENT_ID = os.getenv('GH_CLIENT_ID')
+GITHUB_CLIENT_SECRET = os.getenv('GH_CLIENT_SECRET')
 GCS_BUCKET_NAME = os.getenv('GCS_BUCKET_NAME', 'your-default-bucket-name')
 DATABASE_URL=postgresql+psycopg2://<USER>:<PASSWORD>@/<DB_NAME>?host=/cloudsql/<CONNECTION_NAME>
 
