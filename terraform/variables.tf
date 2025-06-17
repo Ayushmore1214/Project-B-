084bf503464b0ed7d63b72adc0782f76bcd104e8
Ayush More <ayushmore42595@gmail.com>
2025-06-17 15:43:05 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -9,8 +9,19 @@ variable "region" {
   default     = "us-central1"
 }
 
+variable "gcs_bucket_name" {
+  description = "GCS bucket name"
+  type        = string
+}
+
 variable "github_client_id" {
   description = "GitHub OAuth Client ID"
+  type        = string
+  sensitive   = true
+}
+
+variable "github_client_secret" {
+  description = "GitHub OAuth Client Secret"
   type        = string
   sensitive   = true
 }
